c6aab73cb7dc5b6f59d93da081d8dd8c7c4f491f
DanielLoeraMilan <daniel1000an@gmail.com>
2022-05-26 11:56:41 -0500

    Sexto Ejercicio: 30 min.

--- src/test/java/uaslp/objetos/escuela/Exercise6.java
+++ src/test/java/uaslp/objetos/escuela/Exercise6.java
@@ -1,6 +1,8 @@
 package uaslp.objetos.escuela;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -24,5 +26,56 @@ public class Exercise6 {
         assertThat(Dependencia1.class).isInterface();
         assertThat(Dependencia2.class).isInterface();
         assertThat(Dependencia3.class).isInterface();
+    }
+
+    @Test
+    public void givenAAndB_whenAAndBAreEqual_thenSaveStringC(){
+        //Given
+        Dependencia1 dependencia1 = Mockito.mock(Dependencia1.class);
+        Dependencia2 dependencia2 = Mockito.mock(Dependencia2.class);
+        Dependencia3 dependencia3 = Mockito.mock(Dependencia3.class);
+        AlgoritmoX algoritmoX = new AlgoritmoX(dependencia1, dependencia2, dependencia3);
+
+        //When
+        algoritmoX.algoritmoACubrir(1, 1, "Saved");
+
+        //Then
+        Mockito.verify(dependencia1).save("Saved");
+        Mockito.verify(dependencia3).recover();
+        Mockito.verifyNoMoreInteractions(dependencia1, dependencia2, dependencia3);
+    }
+
+    @Test
+    public void givenAAndB_whenAIsSmallerThanB_thenPrintAAndC(){
+        //Given
+        Dependencia1 dependencia1 = Mockito.mock(Dependencia1.class);
+        Dependencia2 dependencia2 = Mockito.mock(Dependencia2.class);
+        Dependencia3 dependencia3 = Mockito.mock(Dependencia3.class);
+        AlgoritmoX algoritmoX = new AlgoritmoX(dependencia1, dependencia2, dependencia3);
+
+        //When
+        algoritmoX.algoritmoACubrir(1, 2, "Saved");
+
+        //Then
+        Mockito.verify(dependencia2).print(1, "Saved");
+        Mockito.verify(dependencia3).recover();
+        Mockito.verifyNoMoreInteractions(dependencia1, dependencia2, dependencia3);
+    }
+
+    @Test
+    public void givenAAndB_whenAIsGreaterThanB_thenSendAAndB(){
+        //Given
+        Dependencia1 dependencia1 = Mockito.mock(Dependencia1.class);
+        Dependencia2 dependencia2 = Mockito.mock(Dependencia2.class);
+        Dependencia3 dependencia3 = Mockito.mock(Dependencia3.class);
+        AlgoritmoX algoritmoX = new AlgoritmoX(dependencia1, dependencia2, dependencia3);
+
+        //When
+        algoritmoX.algoritmoACubrir(2, 1, "Saved");
+
+        //Then
+        Mockito.verify(dependencia3).send(2,1);
+        Mockito.verify(dependencia3).recover();
+        Mockito.verifyNoMoreInteractions(dependencia1, dependencia2, dependencia3);
     }
 }
